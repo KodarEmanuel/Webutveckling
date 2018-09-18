9d74f987388ab7233314ef005542f4937bf7a0ad
Emanuel <info@emanueljohansson.se>
2018-09-18 20:48:56 +0200

    Init Js

---
+++ JavaScript/main.js
@@ -0,0 +1,16 @@
+// function greetMe(name) {
+//     var today = new Date();
+//     alert("hello "+name + "! The date is: "+ today.toDateString());
+// }
+
+// greetMe("World");
+
+function myFunc() {
+    document.getElementById("head").innerHTML = "Hello World";
+}
+function myFunc2() {
+    document.getElementById("paragraf").style.color = "#aaa555";
+}
+function myFunc3() {
+    document.getElementById("pbody").style.color = "#fff111";
+}
